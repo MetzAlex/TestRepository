05660126511200711405d359b46d48e4a71bbeb7
MetzAlex <alexanda.metz@stud.fh-luebeck.de>
2014-12-21 20:02:20 +0100

    Datei geändert
    Change-Id: I9fc3244fbe582d0b630f4a1b84560aac29381786

--- Galgenraten.java
+++ Galgenraten.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+
 public class Galgenraten
 {
 	public static void main(String args[])throws IOException
